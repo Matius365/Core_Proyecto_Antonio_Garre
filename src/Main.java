01f4f65ab77a3f0f25138fdb2e8a7d98626dd25b
Matius365 <macius365@gmail.com>
2026-02-22 13:11:02 +0100

    añadido comentario sobre los cambios

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*Hola. He hecho cambios para poder guardar las emergencias en un fichero json,
+tambien he cambiado el nombre de las clases para que coincidan con la convencion Java,
+y se han refactorizado. Espero que estos cambios funcionen bien.
+ */
+
 public class Main {
     public static void main(String[] args) {
 
